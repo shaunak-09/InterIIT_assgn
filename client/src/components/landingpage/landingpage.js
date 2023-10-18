203077b030b43a457ab71ec0b606272d43e87db4
shaunak-09 <shaunakghosh09@kgpian.iitkgp.ac.in>
2023-10-18 14:47:24 +0530

    .integration

--- client/src/components/landingpage/landingpage.js
+++ client/src/components/landingpage/landingpage.js
@@ -12,11 +12,46 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+function Landingpage(props) 
+ {  
+      const [category, setCategory] = useState('');
+      const [news, setNews] = useState([]);
+      const [orinews, setOriNews] = useState([]);
+      const [searchquery, setSearchquery] = useState("")
+      const [searchquery1,setSearchquery1]=useState("")
+
+     const handleSearch=()=>{
+        getnews({query:{q:searchquery}})
+     }
+     const handleSearch1=(value)=>{
+        if(value=="")
+        setNews(orinews)
+    else {
+        // console.log(value);
+        // console.log(orinews);
+        const data=orinews.filter((item)=>{return (item?.title.toLowerCase().includes(value.toLowerCase()))})
+        setNews(data);
+        console.log(data);
+
+    }
+        
+        setSearchquery1(value);
+        
+     }
+     
 
 
-function Landingpage(props) {
-    const handleclick = () => {
+      const handleclick = () => {
         props.setloginsignup(true);
+      };
+      
+      const handleChange = (event) => {
+        setCategory(event.target.value);
       };
       const logout = () => {
         props.setLogstat(0);
@@ -25,9 +60,6 @@ function Landingpage(props) {
     
         toast.success("Logged out successfully");
       };
-      const [news, setNews] = useState([]);
-      const [orinews, setOriNews] = useState([]);
-
 
       const getnews=({query}) => {
         
@@ -35,39 +67,47 @@ function Landingpage(props) {
             ...query,
             language:"en",
         }
-        console.log(params);
+        // console.log(params);
         axios.get('http://localhost:8000/api/news',{
             params:params
         })
           .then(response =>{
-            console.log(response.data);
+            // console.log(response.data);
              setNews(response.data)
              setOriNews(response.data)
 
           })
           .catch(error => console.log(error));
       };
+      
+      useEffect(()=>{
+        if(category!="")
+        {
+            getnews({query:{category:category}})
+        }
+      },[category])
+
+   
      useEffect(()=>{
         getnews({});
      },[])
     
-     const [searchquery, setSearchquery] = useState("")
-     const handleSearch=()=>{
-        getnews({query:{q:searchquery}})
-     }
+     
 
-  const Card=({data})=>{
+  const Card1=({data})=>{
     // console.log(data);
     return(
-    <Card sx={{ maxWidth: 400 }}>
+    <Card sx={{ maxWidth: 400 }}
+    className="p-2 mx-8 my-4 cursor-pointer ">
         <CardMedia
           component="img"
-          height="140"
+          height="100"
           image={data?.urlToImage}
-          alt="green iguana"
+          alt="image"
+        //   onError={this.src='placeholder.jpg'} 
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h6" component="div">
             {data?.title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
@@ -79,19 +119,36 @@ function Landingpage(props) {
   }
 
   return (
-
         <div className="p-4 flex flex-col justify-center items-center">
+
       {props.logstat == 1 ? (
         <>
           <button
-            class="p-3 bg-blue-500 left-5 top-2 absolute font-bold rounded-xl"
+            class="p-3 mb-2 bg-blue-500 left-5 top-2 absolute font-bold rounded-xl"
             onClick={logout}
           >
             LOGOUT
           </button>
-          <div className="p-4 flex flex-col justify-center items-center">
-            <div>
+          <div className="py-10 px-10 flex flex-col justify-center items-center">
+            <div className="p-2 flex justify-center items-center">
             <TextField
+        id="input-with-icon-textfield"
+        label="Search Headline"
+        value={searchquery1}
+        onChange={(e)=>{
+            handleSearch1(e.target.value)
+        }}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        variant="outlined"
+      />
+                <div className="mx-8 flex justify-center items-center">
+                <TextField
         id="input-with-icon-textfield"
         label="Search By Keywords"
         value={searchquery}
@@ -107,14 +164,38 @@ function Landingpage(props) {
         }}
         variant="outlined"
       />
-      <Button onClick={handleSearch}>Search</Button>
+      <Button variant="outlined" onClick={handleSearch} sx={{mx:1}} className="p-3">Search</Button>
 
+                </div>
+            
+      <Box sx={{ minWidth: 200 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Category</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={category}
+          label="Category"
+          onChange={handleChange}
+        >
+          <MenuItem value="business">Business</MenuItem>
+          <MenuItem value="entertainmen">Entertainment</MenuItem>
+          <MenuItem value="general">General</MenuItem>
+          <MenuItem value="health">Health</MenuItem>
+          <MenuItem value="science">Science</MenuItem>
+          <MenuItem value="sports">Sports</MenuItem>
+          <MenuItem value="technology">Technology</MenuItem>
+
+
+        </Select>
+      </FormControl>
+    </Box>
             </div>
           
       <div className="p-4 flex flex-wrap my-3 ">
         {
           news.map((item,index)=>(
-                <Card data={item} key={`${index}+${index+1}`} />
+                <Card1 data={item} key={`${index}+${index+1}`} />
           ))
 
         }
@@ -135,10 +216,12 @@ function Landingpage(props) {
           </button>
         </>
 
-      )
-}
+       )
+ }
 </div>
-  )
+   )
 }
+  
+
 
 export default Landingpage
--- server/controllers/login.js
+++ server/controllers/login.js
@@ -4,6 +4,7 @@ const bcrypt=require("bcryptjs")
 const login= async (req,res) => {
 
     const { email, password } = req.body;
+    // console.log(req.body);
     const user = await User.findOne({ email });
     if (!user) return res.status(400).send('Invalid email or password');
     // const data =JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
@@ -11,10 +12,10 @@ const login= async (req,res) => {
         if (err) throw err;
     
         if (result) {
-          console.log('Password is correct');
+          // console.log('Password is correct');
           res.json({username:user.username,email});
         } else {
-          console.log('Password is incorrect');
+          // console.log('Password is incorrect');
           res.status(400).json('Invalid email or password');
         }
       });
